dd7dd1dcb04aa8ba911268f77d6494b621b46151
qurrat559 <qurratulain.559@gmail.com>
2025-06-02 16:52:46 +0500

    feat: implement user sign-up functionality with validation and JWT token generation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,15 +1,19 @@
 import { Router } from "express";
+import { signUp } from "../controllers/auth.controller.js";
+
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (req, res) => {
-  res.send({ title: "Sign Up" });
-});
+//Path : /api/v1/auth/sign-up
+authRouter.post("/sign-up", signUp);
 
+
+//Path : /api/v1/auth/sign-in
 authRouter.post("/sign-in", (req, res) => {
   res.send({ title: "Sign In" });
 });
 
+//Path : /api/v1/auth/sign-out
 authRouter.post("/sign-out", (req, res) => {
   res.send({ title: "Sign Out" });
 });
