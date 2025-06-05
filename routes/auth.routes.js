8e53a5c59d5e1a1bbbfb774d15cca8f7113b6469
qurrat559 <qurratulain.559@gmail.com>
2025-06-05 12:12:55 +0500

    feat: implement sign-in and sign-out functionality with error handling

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,17 +1,13 @@
 import { Router } from "express";
-import { signUp } from "../controllers/auth.controller.js";
-
+import { signUp, signIn } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
 //Path : /api/v1/auth/sign-up
 authRouter.post("/sign-up", signUp);
 
-
 //Path : /api/v1/auth/sign-in
-authRouter.post("/sign-in", (req, res) => {
-  res.send({ title: "Sign In" });
-});
+authRouter.post("/sign-in", signIn);
 
 //Path : /api/v1/auth/sign-out
 authRouter.post("/sign-out", (req, res) => {
