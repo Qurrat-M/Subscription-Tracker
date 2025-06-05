b82cac6e01bc78e41ac2445dabe6d69cb1fd6da3
qurrat559 <qurratulain.559@gmail.com>
2025-06-05 15:40:14 +0500

    feat: implement user retrieval and authorization middleware

--- models/user.model.js
+++ models/user.model.js
@@ -17,15 +17,12 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       minLength: 5,
       maxLength: 255,
-      match: [
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-        "Please enter a valid email address",
-      ],
+      match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
     },
     password: {
       type: String,
       required: [true, "Password is required"],
-      minLength: 6,
+      minLength: [6, "Password must be at least 6 characters long"],
     },
   },
   { timestamps: true }
