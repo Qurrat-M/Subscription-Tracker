65cbe6354acccab64fb1742cd406df014793ba57
Qurrat-M <qurratulain.559@gmail.com>
2025-09-16 16:40:26 +0500

    integrate upstash workflow, enable workflow route, add user subscribers API

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,4 +1,6 @@
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
+import { SERVER_URL } from '../config/env.js'
 
 export const createSubscription = async (req, res, next) => {
   try {
@@ -6,11 +8,41 @@ export const createSubscription = async (req, res, next) => {
       ...req.body,
       user: req.user._id,
     });
-    res.status(201).json({
+
+    const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+      retries: 0,
+    })
+
+    res.status(201).json({ success: true, data: { subscription, workflowRunId } });
+  } catch (e) {
+    next(e);
+  }
+}
+
+export const getUserSubscriptions = async (req, res, next) => {
+  try {
+    //check if user is authorized to access this resource
+    if (req.user.id !== req.params.id) {
+      const error = new Error("You are not the owner of this account");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    const subscriptions = await Subscription.find({ user: req.params.id });
+
+    res.status(200).json({
       success: true,
-      message: "Subscription created successfully",
-      data: subscription,
+      message: "Subscriptions fetched successfully",
+      data: subscriptions,
     });
+
   } catch (error) {
     next(error);
   }
